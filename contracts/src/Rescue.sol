10c480ae1728aa9c89ced842a28df425464df95d
verumlotus <universumtremendum@gmail.com>
2022-08-21 14:36:32 -0700

    Resolved submodules

---
+++ contracts/src/Rescue.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/**
+ * @title Rescue
+ * @notice Simple interface to help rescue assets in case of private key theft
+ * @author Verum, John, Lyron
+ */
+contract Rescue {
+    /************************************************
+     *  STORAGE
+    ***********************************************/
+
+    /************************************************
+     *  IMMUTABLES & CONSTANTS
+    ***********************************************/
+
+    /************************************************
+     *  EVENTS, ERRORS, MODIFIERS
+    ***********************************************/
+
+    /**
+     * @notice Rescue function 
+    */
+    function rescueAssets(address[] calldata erc20Addresses, address backupAddress) public {
+
+    }
+
+
+}
