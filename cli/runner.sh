66c40329d80acf0cac71af5bac56a9ed8161b676
jtguibas <jtguibas@gmail.com>
2022-08-21 21:36:38 -0700

    boom

--- cli/runner.sh
+++ cli/runner.sh
@@ -1,9 +1,8 @@
-rm presigned.csv
-rm approves.csv
 cargo run --release -- \
   --private-key $HACKLODGE_PRIVATE_KEY \
   --backup-address $HACKLODGE_BACKUP_ADDRESS \
   --contract-address $HACKLODGE_CONTRACT_ADDRESS \
   --min-gas 10 \
   --max-gas 100 \
-  --gas-step 10
+  --gas-step 10 \
+  --nonce 0
