7e8911417e173dbcb3e4aa5333d104b7051c2dad
jtguibas <jtguibas@gmail.com>
2022-08-21 21:16:59 -0700

    cli tool support

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,6 @@
+use clap::Parser;
 use ethers::{prelude::*, types::transaction::eip2718::TypedTransaction};
 use eyre::Result;
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::sync::Arc;
@@ -11,15 +11,27 @@ use watchtower::Watchtower;
 mod erc20;
 use erc20::ERC20;
 
-const GAS_STEP: usize = 10;
-const MIN_GAS: usize = 10;
-const MAX_GAS: usize = 100;
-const HTTP_PROVIDER: &str = "https://mainnet.infura.io/v3/24cbf7ab0f8c4621ab876e6b67b68a3d";
 const ERC20_ADDRESSES: [&str; 3] = [
-    "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
-    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
-    "0xA808B22ffd2c472aD1278088F16D4010E6a54D5F",
+    "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", // Uniswap
+    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", // Aave
+    "0xA808B22ffd2c472aD1278088F16D4010E6a54D5F", // ReFi
 ];
+
+#[derive(Parser, Default, Debug)]
+struct Arguments {
+    #[clap(long)]
+    private_key: String,
+    #[clap(long)]
+    backup_address: String,
+    #[clap(long)]
+    contract_address: String,
+    #[clap(long)]
+    min_gas: usize,
+    #[clap(long)]
+    max_gas: usize,
+    #[clap(long)]
+    gas_step: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -32,15 +44,22 @@ async fn main() -> Result<()> {
     //     .write_to_file("erc20.rs")?;
 
     // Setup
-    let address = env::var("HACKLODGE_ADDRESS")?.parse::<Address>()?;
-    let backup_address = env::var("HACKLODGE_BACKUP_ADDRESS")?.parse::<Address>()?;
-    let private_key = env::var("HACKLODGE_PRIVATE_KEY")?;
+    let args = Arguments::parse();
+    let private_key = args.private_key;
     let wallet = private_key.parse::<LocalWallet>()?;
-    let provider = Provider::<Http>::try_from(HTTP_PROVIDER)?;
+    let user_address: Address = wallet.address();
+    let backup_address = args.backup_address.parse::<Address>()?;
+    let contract_address = args.contract_address.parse::<Address>()?;
+
+    let min_gas = args.min_gas;
+    let max_gas = args.max_gas;
+    let gas_step = args.gas_step;
+
+    let provider = Provider::<Http>::try_from("http://x.com")?;
     let client = Arc::new(SignerMiddleware::new(provider, wallet));
 
     // Generate calldata
-    let contract = Watchtower::new(address, client.clone());
+    let contract = Watchtower::new(contract_address, client.clone());
     let tx = contract.rescue_assets(
         ERC20_ADDRESSES
             .map(|s| s.parse::<Address>().unwrap())
@@ -53,11 +72,10 @@ async fn main() -> Result<()> {
     // Presign rescue transactions
     let mut buffer = File::create("presigned.csv")?;
     for nonce in 0..1000 {
-        for gas_price in (MIN_GAS..MAX_GAS).step_by(GAS_STEP) {
+        for gas_price in (min_gas..max_gas).step_by(gas_step) {
             let tx: TypedTransaction = TransactionRequest::new()
-                .to(address)
-                .value(1000)
-                .from(address)
+                .to(backup_address)
+                .from(user_address)
                 .nonce(nonce)
                 .data(data.clone())
                 .gas_price(gas_price)
@@ -75,13 +93,13 @@ async fn main() -> Result<()> {
     ERC20_ADDRESSES.map(|s| {
         let erc20_address = s.parse::<Address>().unwrap();
         let contract = ERC20::new(erc20_address, client.clone());
-        let tx = contract.approve(address, U256::max_value());
+        let tx = contract.approve(contract_address, U256::max_value());
         let tx = tx.tx.as_eip1559_ref().unwrap();
         let data = tx.data.as_ref().unwrap().clone();
 
         let tx: TypedTransaction = TransactionRequest::new()
             .to(erc20_address)
-            .from(address)
+            .from(user_address)
             .data(data)
             .into();
 
