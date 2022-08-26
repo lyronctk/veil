378f70c853de7f7989c8e926efac3b4c5be11aca
Lyron Co Ting Keh <lyronctk@stanford.edu>
2022-08-26 14:09:07 -0700

    old pages gone

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
+import { ethers } from "ethers";
 
 import * as React from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { useSigner } from "wagmi";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ConnectButton, useConnectModal } from "@rainbow-me/rainbowkit";
 
 import Papa from "papaparse";
 
@@ -23,6 +24,7 @@ export default function Home() {
 
   const [upApproveStat, setUpApproveStat] = React.useState(0);
   const [upRescueStat, setUpRescueStat] = React.useState(0);
+  const { openConnectModal } = useConnectModal();
 
   // Update the displayed CLI command when wallet is connected
   useEffect(() => {
@@ -33,9 +35,9 @@ export default function Home() {
     const signerAddr = await signer.getAddress();
     fetch(`${SERVER_ENDPOINT}/heldERC20/${signerAddr}`)
       .then(async (res) => {
-        const quantParam = `--min-gas 10 --max-gas 100 --gas-step 10 --nonce ${txCt}`
+        const quantParam = `--min-gas 10 --max-gas 100 --gas-step 10 --nonce ${txCt}`;
         const heldAddresses = await res.json();
-        const strAddresses = heldAddresses.join(' ');
+        const strAddresses = heldAddresses.join(" ");
         const tokenParam = `--erc20-addresses ${strAddresses}`;
         setCliCmd(
           `watchtower ${CLI_USER} ${CLI_RESCUE} ${quantParam} ${tokenParam} ${CLI_OUT}`
@@ -44,7 +46,7 @@ export default function Home() {
       .catch((e) => console.error(e));
   };
 
-  // Send signed approval / rescue transactions to backend to be stored 
+  // Send signed approval / rescue transactions to backend to be stored
   const uploadSignatures = async (event) => {
     const chunk = (a, size) =>
       Array.from(new Array(Math.ceil(a.length / size)), (_, i) =>
@@ -53,47 +55,33 @@ export default function Home() {
 
     Papa.parse(event.target.files[0], {
       header: true,
-      skipEmptyLines: true,
+      skipEmptyLines: true, 
       complete: async (results) => {
         // Send approval transactions
-        // const approvals = results.data
-        //   .filter((row) => row["type"] === "approve")
-        // approvals = approvals.slice(0, 1); // [DEBUG]
-        // const approveRequestOptions = {
-        //   method: "POST",
-        //   headers: { "Content-Type": "application/json" },
-        //   body: JSON.stringify({approveData: approvals}),
-        // };
-        // fetch(`${SERVER_ENDPOINT}/postApproveTxs`, approveRequestOptions)
-        //   .then((response) => setUpApproveStat(response.status))
-        //   .catch((e) => console.error(e));
+        const approvals = results.data.filter(
+          (row) => row["type"] === "approve"
+        );
+        const approveRequestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ approveData: approvals }),
+        };
+        fetch(`${SERVER_ENDPOINT}/postApproveTxs`, approveRequestOptions)
+          .then((response) => setUpApproveStat(response.status))
+          .catch((e) => console.error(e));
 
-        // const rescueTxs = results.data
-        //   .filter((row) => row["type"] === "rescue")
-        //   .map(({ type, ...others }) => {
-        //     return others;
-        //   });
-
-        // [TMP]
-        const signerAddr = await signer.getAddress();
-        if (!signerAddr) return;
+        // Send rescue transactions
         const rescueTxs = results.data
           .filter((row) => row["type"] === "rescue")
           .map((row) => {
-            row["userAddress"] = signerAddr; 
-            row["gasPrice"] = parseInt(row["gasPrice"]) ; 
-            return row
+            row["gasPrice"] = parseInt(row["gasPrice"]);
+            return row;
           });
-        rescueTxs = rescueTxs.slice(0, 1);
-        console.log(rescueTxs);
-        //
-
-        // Send rescue transactions
         chunk(rescueTxs, 100).map((rescueChunk) => {
           const rescueRequestOptions = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({signedRescueTxs: rescueChunk}),
+            body: JSON.stringify({ signedRescueTxs: rescueChunk }),
           };
           fetch(`${SERVER_ENDPOINT}/postRescueTxs`, rescueRequestOptions)
             .then((response) => {
@@ -107,91 +95,97 @@ export default function Home() {
     });
   };
 
+  const [latestTxHash, setLatestTxHash] = React.useState("");
+  const [mempool, setMempool] = React.useState([]);
+
+  var url =
+    "wss://eth-goerli.g.alchemy.com/v2/zg3KpYV3WSXDhGlkzW3KdupZfG40nW2e";
+  var customWsProvider = new ethers.providers.WebSocketProvider(url);
+  customWsProvider.on("pending", (tx) => {
+    setLatestTxHash(tx);
+  });
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>WT Management</title>
-        {/* <meta name="description" content="Learn forms with Next.js" /> */}
+        <title>Watchtower ⚡</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div style={{ height: "20px", backgroundColor: "yellow" }}></div>
+      <main className="container max-w-2xl ml-10 matter-regular">
+        <br></br>
+        <div>
+          <div className="matter-heavy text-3xl">
+            Watchtower
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-9 h-9 inline pb-2"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div>Your ultimate defense against private key theft.</div>
+          <br></br>
+          <div>
+            Watchtower protects assets by frontrunning unauthorized transactions
+            and transferring all your assets to a secure, pre-specificied backup
+            address. It can be set up in seconds, without revealing your private
+            key at any point in the process.
+          </div>
+          <br></br>
+          <div className="matter-heavy text-lg">Scanning the mempool...</div>
+          {latestTxHash}
+          <br></br>
+          <br></br>
+          <div className="matter-heavy text-lg">How do I get started?</div>
+          <div>
+            1) Connect your wallet.
+            <div className="my-3">
+              <ConnectButton></ConnectButton>
+            </div>
+          </div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Launching your <a href="https://nextjs.org">Watchtower</a>
-        </h1>
-        <h1></h1>
-        <h1></h1>
+          <div>
+            2) Save your generated watchtower command.
+            <br></br>
+            <div className="my-3 bg-black text-white px-5 py-3">
+              {cliCmd ||
+                `watchtower --private-key $PRIV_KEY \
+              --backup-address $BACKUP_ADDR \
+              --contract-address 0x8F8F457a0F6BF163af60bC9254E47a44E01AD776 \
+              --min-gas 10 \
+              --max-gas 100 \
+              --gas-step 10 \
+              --nonce 8 \
+              --erc20-addresses 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60 \
+              --output-path not-your-private-keys.csv`}
+            </div>
+          </div>
 
-        <h2>[1] Connect you wallet</h2>
-        <ConnectButton />
-        <p></p>
-        <p></p>
+          <div>
+            3) Run watchtower from a secure sandbox and upload your signed
+            transactions.
+            <input
+              type="file"
+              name="Signed Transactions"
+              accept=".csv"
+              onChange={uploadSignatures}
+              className="my-3"
+            />
+            <p>Approve Upload Status: {upApproveStat}</p>
+            <p>Rescue Upload Status: {upRescueStat}</p>
+          </div>
+          <br></br>
 
-        <h2>[2] Save your generated cli command</h2>
-        <code className={styles.code}>{cliCmd || "wallet not connected"}</code>
-        <p></p>
-        <p></p>
-
-        <h2>
-          [3] Run <code className={styles.code}>watchtower</code> from a secure
-          sandbox
-        </h2>
-        <p>
-          <em>
-            don't substitute your private key and run our executable until
-            you've sealed off your machine from wifi / bluetooth / radio /
-            lights / air / hopes / dreams that could steal your info.
-          </em>
-        </p>
-        <p>No worries, we'll wait!</p>
-        <p></p>
-        <p></p>
-
-        <h2>[4] Upload your signed transactions</h2>
-        <p>
-          <em>Hint: we don't need your private key here</em>
-        </p>
-        <input
-          type="file"
-          name="Signed Transactions"
-          accept=".csv"
-          onChange={uploadSignatures}
-          style={{ display: "block", margin: "10px auto" }}
-        />
-        <p>approve upload status: {upApproveStat}</p>
-        <p>rescue upload status: {upRescueStat}</p>
-        <p></p>
-        <p></p>
-
-        <h2>
-          [5] Verify that all assets you care about are on our Rescue List
-        </h2>
-        <p>
-          <em>sleep soundly now that your assets are secure</em>
-        </p>
-        <ul>
-          <li>UNI</li>
-          <li>MKR</li>
-        </ul>
-        <p></p>
-        <p></p>
-        <h3></h3>
-
-        <h2>[6] We'll alert you here for any breaches</h2>
-        <p>
-          <em>Once we've escorted your assets to safety</em>
-        </p>
-        <p>Assets safe, no thefts detected</p>
-        <p></p>
-        <p></p>
-        <h3></h3>
+          <div>4) Sleep soundly now that your assets are protected :)</div>
+        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
-          Built with Next.js | Powered by Watchtower
-        </a>
-      </footer>
     </div>
   );
 }
